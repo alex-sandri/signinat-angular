bfaedf7a62112875846071909d62b1def471c9c1
Alex Sandri <me@alexsandri.com>
2020-11-04 14:24:15 +0100

    Redirect to signin route

--- src/app/guards/auth-guard/auth-guard.guard.ts
+++ src/app/guards/auth-guard/auth-guard.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SettingsService } from 'src/app/services/settings/settings.service';
 
@@ -7,10 +7,17 @@ import { SettingsService } from 'src/app/services/settings/settings.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return SettingsService.exists("session");
+    if (!SettingsService.exists("session"))
+    {
+      this.router.navigate([ "signin" ]);
+    }
+
+    return true;
   }
   
 }
