9652eac55d31dd4069ac6368bfdcc3a07fd444e3
Alex Sandri <me@alexsandri.com>
2020-11-04 14:09:36 +0100

    Create 'createNewAppFormOnSubmit'

--- src/app/account/settings/settings.component.ts
+++ src/app/account/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  @ViewChild("createNewAppDialog") createNewAppDialog!: ElementRef<HTMLDialogElement>;
+  @ViewChild("createNewAppForm") createNewAppForm!: ElementRef<HTMLFormElement>;
+
   section: "profile" | "developer" = "profile";
+
+  setCreateNewAppDialogVisible(visible: boolean) {
+    if (visible)
+    {
+      this.createNewAppDialog.nativeElement.showModal();
+    }
+    else
+    {
+      this.createNewAppDialog.nativeElement.close();
+
+      this.createNewAppForm.nativeElement.reset();
+    }
+  }
+
+  createNewAppFormOnSubmit(e: Event, name: string, url: string) {
+    e.preventDefault();
+
+    console.log(name, url);
+  }
 
   constructor() { }
 
