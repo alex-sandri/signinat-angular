a3c7afd4cd6281e341f35d97c2ba1eacdd560940
Alex Sandri <me@alexsandri.com>
2020-11-04 18:53:02 +0100

    Hide home CTA if the user is signed in

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-index',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  isSignedIn: boolean;
+
+  constructor(authService: AuthService) {
+    this.isSignedIn = authService.isSignedIn();
+  }
 
   ngOnInit(): void {
   }
