aa8daa6222359761899db8440b8d47d2b2fe2afb
Alex Sandri <me@alexsandri.com>
2020-11-04 09:50:45 +0100

    Implement SignIn route

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ISerializedSession } from 'api/src/models/Session';
+import { ApiService } from '../services/api/api.service';
+import { SettingsService } from '../services/settings/settings.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  email: string = "";
+  password: string = "";
+
+  emailError: string = "";
+  passwordError: string = "";
+
+  setEmail = (value: string) => { this.email = value; }
+  setPassword = (value: string) => { this.password = value; }
+
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+  }
+
+  async onSubmit(e: Event, form: HTMLFormElement): Promise<void> {
+    e.preventDefault();
+
+    const submitButton = form.querySelector("button[type=submit]") as HTMLButtonElement;
+
+    submitButton.disabled = true;
+
+    this.emailError = this.passwordError = "";
+
+    const response = await this.api.createSession({
+      email: this.email.trim(),
+      password: this.password,
+    });
+
+    if (!response.result.valid)
+    {
+      this.emailError = response.errors.email.error;
+      this.passwordError = response.errors.password.error;
+    }
+    else
+    {
+      SettingsService.set("session", (response.result.data as ISerializedSession).id);
+
+      location.href = "/account";
+    }
+
+    submitButton.disabled = false;
   }
 
 }
