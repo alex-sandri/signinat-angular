e1aebe20a709defee3b3f647f0c0008dc9eda8e7
Alex Sandri <me@alexsandri.com>
2020-11-14 14:42:45 +0100

    Remove duplicates

--- api/src/models/Scope.ts
+++ api/src/models/Scope.ts
@@ -33,7 +33,7 @@ export class Scope
         description: this.description,
         includes: Scope
             .all()
-            .filter(scope => scope.value.startsWith(`${this.value}.`))
+            .filter(scope => scope.value.match(RegExp(`^${this.value}\.[^\.]+$`)))
             .map(scope => scope.json()),
     });
 
