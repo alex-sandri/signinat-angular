f9fca81b335a80499db9488b5f70d0b339691062
Alex Sandri <me@alexsandri.com>
2020-11-15 15:03:16 +0100

    Create AppToken

---
+++ api/src/models/AppToken.ts
@@ -0,0 +1,60 @@
+import { firestore } from "firebase-admin";
+import { v4 as uuidv4 } from "uuid";
+
+import { App } from "./App";
+import { User } from "./User";
+
+const db = firestore();
+
+interface IAppToken
+{
+    app: string,
+    user: string,
+}
+
+export class AppToken
+{
+    constructor(
+        public readonly app: App,
+        public readonly user: User,
+    ) {}
+
+    public static async create(app: App, user: User): Promise<string>
+    {
+        const uuid = uuidv4();
+
+        await db.collection("tokens").doc(uuid).set(<IAppToken>{
+            app: app.id,
+            user: user.id,
+        });
+
+        return uuid;
+    }
+
+    public static async retrieve(token?: string): Promise<AppToken | null>
+    {
+        if (!token) return null;
+
+        const snapshot = await db.collection("tokens").doc(token).get();
+
+        if (!snapshot.exists) return null;
+
+        const data = snapshot.data() as IAppToken;
+
+        const app = await App.retrieve(data.app);
+
+        if (!app) return null;
+
+        const user = await User.retrieve(data.user);
+
+        if (!user) return null;
+
+        // TODO
+        // Filter data based on permission level
+
+        return new AppToken(
+            app,
+            user,
+        );
+    }
+}
