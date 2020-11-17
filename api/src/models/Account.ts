07291a037468378082b08dd9371a99a9fa8a10d2
Alex Sandri <me@alexsandri.com>
2020-11-17 13:43:41 +0100

    Move accounts to a top-level collection

--- api/src/models/Account.ts
+++ api/src/models/Account.ts
@@ -2,32 +2,36 @@ import { firestore } from "firebase-admin";
 
 import { ApiError } from "./ApiError";
 import { App, ISerializedApp } from "./App";
-import { User } from "./User";
+import { ISerializedUser, User } from "./User";
 
 const db = firestore();
 
 interface IAccount
 {
     app: string,
+    user: string,
 }
 
 export interface ISerializedAccount
 {
     id: string,
     app: ISerializedApp,
+    user: ISerializedUser,
 }
 
 export class Account
 {
     private constructor(
-        private readonly id: string,
-        private readonly app: App,
+        public readonly id: string,
+        public readonly app: App,
+        public readonly user: User,
     ) {}
 
     public json = (): ISerializedAccount =>
     ({
         id: this.id,
         app: this.app.json(),
+        user: this.user.json(),
     });
 
     static create = async (user: User, id: string): Promise<Account> =>
@@ -36,19 +40,21 @@ export class Account
 
         if ((await Account.withAppId(user, id)) !== null) throw new ApiError("account/already-exists");
 
-        const account = await db.collection(`users/${user.id}/accounts`).add(<IAccount>{
+        const account = await db.collection("accounts").add(<IAccount>{
             app: id,
+            user: user.id,
         });
 
         return new Account(
             account.id,
             await App.retrieve(id) as App,
+            user,
         );
     }
 
     static retrieve = async (user: User, id: string): Promise<Account | null> =>
     {
-        const account = await db.collection(`users/${user.id}/accounts`).doc(id).get();
+        const account = await db.collection("accounts").doc(id).get();
 
         if (!account.exists) return null;
 
@@ -57,12 +63,13 @@ export class Account
         return new Account(
             id,
             await App.retrieve(data.app) as App,
+            user,
         );
     }
 
     static list = async (user: User): Promise<Account[]> =>
     {
-        const snapshot = await db.collection(`users/${user.id}/accounts`).get();
+        const snapshot = await db.collection("accounts").where("user", "==", user.id).get();
 
         const accounts: Account[] = [];
 
@@ -73,13 +80,14 @@ export class Account
             accounts.push(new Account(
                 account.id,
                 await App.retrieve(data.app) as App,
+                user,
             ));
         }
 
         return accounts;
     }
 
-    static unlink = async (user: User, id: string): Promise<void> => { await db.collection(`users/${user.id}/accounts`).doc(id).delete(); }
+    static unlink = async (user: User, id: string): Promise<void> => { await db.collection("accounts").doc(id).delete(); }
 
     static delete = async (user: User, id: string): Promise<void> =>
     {
@@ -93,7 +101,12 @@ export class Account
 
     static withAppId = async (user: User, app: string): Promise<Account | null> =>
     {
-        const result = await db.collection(`users/${user.id}/accounts`).where("app", "==", app).limit(1).get();
+        const result = await db
+            .collection("accounts")
+            .where("user", "==", user.id)
+            .where("app", "==", app)
+            .limit(1)
+            .get();
 
         if (result.empty) return null;
 
