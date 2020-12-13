d6acd31eb1ed0d1fc2a982a7474698a9e56b3a6f
Alex Sandri <me@alexsandri.com>
2020-12-13 19:08:21 +0100

    Add more conditions

--- api/src/utilities/Validator.ts
+++ api/src/utilities/Validator.ts
@@ -1,5 +1,9 @@
+import * as dayjs from "dayjs";
+
 import { ApiError, ISerializedApiError, TApiErrorType } from "../models/ApiError";
 import { User } from "../models/User";
+
+dayjs.extend(require("dayjs/plugin/customParseFormat"));
 
 type TValidatorType = "create" | "update";
 
@@ -48,6 +52,13 @@ export class Validator
             return result;
         }
 
+        if (user.birthday)
+        {
+            if (user.birthday.length === 0) result.add("user/birthday/empty");
+            else if (typeof user.birthday !== "string") result.add("user/birthday/invalid");
+            else if (!dayjs(user.birthday, "YYYY-MM-DD")) result.add("user/birthday/invalid");
+        }
+
         switch (this.type)
         {
             case "create":
@@ -55,38 +66,44 @@ export class Validator
                 else
                 {
                     if (!user.name.first) result.add("user/name/first/required");
+                    else if (typeof user.name.first !== "string") result.add("user/name/first/invalid");
                     else if (user.name.first.length === 0) result.add("user/name/first/empty");
 
                     if (!user.name.last) result.add("user/name/last/required");
+                    else if (typeof user.name.last !== "string") result.add("user/name/last/invalid");
                     else if (user.name.last.length === 0) result.add("user/name/last/empty");
                 }
 
                 if (!user.email) result.add("user/email/required");
+                else if (typeof user.email !== "string") result.add("user/email/invalid");
                 else if (user.email.length === 0) result.add("user/email/empty");
                 else if (await User.exists(user.email)) result.add("user/email/already-exists");
 
                 if (!user.password) result.add("user/password/required");
+                else if (typeof user.password !== "string") result.add("user/password/invalid");
                 else if (user.password.length === 0) result.add("user/password/empty");
                 else if (user.password.length < 8) result.add("user/password/weak");
                 break;
             case "update":
-                if (user.name?.first)
+                // TODO: Add else case for invalid types
+
+                if (typeof user.name?.first === "string")
                 {
                     if (user.name.first.length === 0) result.add("user/name/first/empty");
                 }
 
-                if (user.name?.last)
+                if (typeof user.name?.last === "string")
                 {
                     if (user.name.last.length === 0) result.add("user/name/last/empty");
                 }
 
-                if (user.email)
+                if (typeof user.email === "string")
                 {
                     if (user.email.length === 0) result.add("user/email/empty");
                     else if (await User.exists(user.email)) result.add("user/email/already-exists");
                 }
 
-                if (user.password)
+                if (typeof user.password === "string")
                 {
                     if (user.password.length === 0) result.add("user/password/empty");
                     else if (user.password.length < 8) result.add("user/password/weak");
