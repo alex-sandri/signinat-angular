e82d613a6f0cda6427ea3738ebf929ce89d511c9
Alex Sandri <me@alexsandri.com>
2020-12-20 19:05:16 +0100

    Add spinner in manage app and account

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements AfterViewInit
+{
+  @Input()
+  fill: boolean = false;
 
-  constructor() { }
+  @ViewChild("element")
+  element?: ElementRef<HTMLDivElement>;
 
-  ngOnInit(): void {
+  constructor()
+  {}
+
+  ngAfterViewInit(): void
+  {
+    if (this.fill)
+    {
+      this.element?.nativeElement.parentElement!.classList.add("fill");
+    }
   }
-
 }
