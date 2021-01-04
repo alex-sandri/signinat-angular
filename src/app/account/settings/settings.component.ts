1976a8d09aa4483f6d69efae599bf6725ec25c75
Alex Sandri <me@alexsandri.com>
2021-01-04 11:54:50 +0100

    Migrate createNewApp and updateProfile forms to Forms.ts

--- src/app/account/settings/settings.component.ts
+++ src/app/account/settings/settings.component.ts
@@ -5,61 +5,26 @@ import { ISerializedUser } from 'api/src/models/User';
 import { FormComponent, FormOptions, IDateFormInput, ISelectFormInput, ITextFormInput } from 'src/app/components/form/form.component';
 import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import Forms from 'src/config/Forms';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit
+export class SettingsComponent
 {
   user?: ISerializedUser = this.auth.user;
-
-  createNewAppFormOptions: FormOptions = {
-    name: "Create new App",
-    groups: [
-      {
-        name: "default",
-        inputs: [
-          { label: "App Name", name: "name", type: "text", required: true },
-          { label: "URL", name: "url", type: "url", required: true },
-          { label: "Scopes", name: "scopes", type: "select", required: true, options: { multiple: true }, },
-        ],
-      },
-    ],
-    submitButtonText: "Create",
-    showCancelButton: true,
-    hidden: true,
-  };
-
-  updateProfileFormOptions: FormOptions = {
-    name: "Profile",
-    groups: [
-      {
-        name: "default",
-        inputs: [
-          { label: "First Name", name: "first-name", type: "text", required: true, autocomplete: "given-name", value: this.user?.name.first },
-          { label: "Last Name", name: "last-name", type: "text", required: true, autocomplete: "family-name", value: this.user?.name.last },
-          { label: "Email", name: "email", type: "email", required: true, autocomplete: "email", value: this.user?.email },
-        ],
-      },
-      {
-        name: "Additional information",
-        inputs: [
-          { label: "Birthday", name: "birthday", type: "date", required: false, autocomplete: "bday", value: this.getBirthdayAsDate() },
-        ],
-      },
-    ],
-    submitButtonText: "Update",
-    showCancelButton: true,
-    hidden: true,
-  };
 
   @ViewChild("createNewAppForm")
   createNewAppForm!: FormComponent;
 
   @ViewChild("updateProfileForm")
   updateProfileForm!: FormComponent;
+
+  createNewAppFormConfig = Forms.createNewApp(this.api, this.createNewAppForm);
+
+  updateProfileFormConfig = Forms.updateProfile(this.api, this.user as ISerializedUser, this.updateProfileForm, this.getBirthdayAsDate());
 
   section: string;
 
@@ -105,62 +70,6 @@ export class SettingsComponent implements OnInit
     });
   }
 
-  async createNewAppFormOnSubmit(end: () => void)
-  {
-    const response = await this.api.createApp({
-      name: (this.createNewAppFormOptions.groups[0].inputs[0] as ITextFormInput).value,
-      url: (this.createNewAppFormOptions.groups[0].inputs[1] as ITextFormInput).value,
-      scopes: (this.createNewAppFormOptions.groups[0].inputs[2] as ISelectFormInput).selectedValues,
-    });
-
-    if (response.errors)
-    {
-      this.createNewAppFormOptions.groups[0].inputs[0].error = response.errors.find(e => e.id.startsWith("app/name/"))?.message;
-      this.createNewAppFormOptions.groups[0].inputs[1].error = response.errors.find(e => e.id.startsWith("app/url/"))?.message;
-    }
-    else
-    {
-      this.createNewAppForm.hide();
-    }
-
-    end();
-  }
-
-  async updateProfileFormOnSubmit(end: () => void)
-  {
-    const birthday = (this.updateProfileFormOptions.groups[1].inputs[0] as IDateFormInput).value;
-
-    const response = await this.api.updateUser({
-      name: {
-        first: (this.updateProfileFormOptions.groups[0].inputs[0] as ITextFormInput).value,
-        last: (this.updateProfileFormOptions.groups[0].inputs[1] as ITextFormInput).value,
-      },
-      email: (this.updateProfileFormOptions.groups[0].inputs[2] as ITextFormInput).value,
-      birthday: birthday && (`${
-        birthday.getFullYear()
-      }/${
-        (birthday.getMonth() + 1).toString().padStart(2, "0")
-      }/${
-        (birthday.getDate()).toString().padStart(2, "0")
-      }`),
-    });
-
-    if (response.errors)
-    {
-      this.updateProfileFormOptions.groups[0].inputs[0].error = response.errors.find(e => e.id.startsWith("user/name/first/"))?.message;
-      this.updateProfileFormOptions.groups[0].inputs[1].error = response.errors.find(e => e.id.startsWith("user/name/last/"))?.message;
-      this.updateProfileFormOptions.groups[0].inputs[2].error = response.errors.find(e => e.id.startsWith("user/email/"))?.message;
-
-      this.updateProfileFormOptions.groups[1].inputs[0].error = response.errors.find(e => e.id.startsWith("user/birthday/"))?.message;
-    }
-    else
-    {
-      this.updateProfileForm.hide();
-    }
-
-    end();
-  }
-
   constructor(private auth: AuthService, private api: ApiService, private router: Router)
   {
     this.section = router.url.split("/").pop() as string;
@@ -171,9 +80,6 @@ export class SettingsComponent implements OnInit
 
     api
       .listScopes()
-      .then(response => (this.createNewAppFormOptions.groups[0].inputs[2] as ISelectFormInput).selectOptions = response.data);
+      .then(response => (this.createNewAppFormConfig.options.groups[0].inputs[2] as ISelectFormInput).selectOptions = response.data);
   }
-
-  ngOnInit(): void
-  {}
 }
--- src/config/Forms.ts
+++ src/config/Forms.ts
@@ -1,54 +1,160 @@
 import { ActivatedRoute, Router } from "@angular/router";
-import { FormConfig } from "src/app/components/form/form.component";
+import { ISerializedUser } from "api/src/models/User";
+import { FormComponent, FormConfig } from "src/app/components/form/form.component";
 import { ApiService } from "src/app/services/api/api.service";
 import { SettingsService } from "src/app/services/settings/settings.service";
 
 export default class Forms
 {
-    public static readonly signIn: (api: ApiService, router: Router, route: ActivatedRoute, settings: SettingsService) => FormConfig
-        = (api: ApiService, router: Router, route: ActivatedRoute, settings: SettingsService) =>
-            ({
-                options: {
-                    name: "Sign In",
-                    groups: [
-                        {
-                            name: "default",
-                            inputs: [
-                                { label: "Email", name: "email", type: "email", required: true, autocomplete: "email" },
-                                { label: "Password", name: "password", type: "password", required: true, autocomplete: "current-password" },
-                            ],
-                        },
-                    ],
-                    submitButtonText: "Sign In",
-                },
-                onSubmit: async (data, options, end) =>
-                {
-                    const response = await api.createUserToken({
-                        email: data["email"],
-                        password: data["password"],
-                    });
-
-                    if (response.errors)
+    public static signIn(api: ApiService, router: Router, route: ActivatedRoute, settings: SettingsService): FormConfig
+    {
+        return {
+            options: {
+                name: "Sign In",
+                groups: [
                     {
-                        options.groups[0].inputs[0].error = response.errors.find(e => e.id.startsWith("token/email/"))?.message;
-                        options.groups[0].inputs[1].error = response.errors.find(e => e.id.startsWith("token/password/"))?.message;
+                        name: "default",
+                        inputs: [
+                            { label: "Email", name: "email", type: "email", required: true, autocomplete: "email" },
+                            { label: "Password", name: "password", type: "password", required: true, autocomplete: "current-password" },
+                        ],
+                    },
+                ],
+                submitButtonText: "Sign In",
+            },
+            onSubmit: async (data, options, end) =>
+            {
+                const response = await api.createUserToken({
+                    email: data["email"],
+                    password: data["password"],
+                });
+
+                if (response.errors)
+                {
+                    options.groups[0].inputs[0].error = response.errors.find(e => e.id.startsWith("token/email/"))?.message;
+                    options.groups[0].inputs[1].error = response.errors.find(e => e.id.startsWith("token/password/"))?.message;
+                }
+                else
+                {
+                    settings.set("session.token", response.data.id);
+                    settings.set("session.userId", response.data.user.id);
+
+                    if (route.snapshot.queryParams["ref"])
+                    {
+                        router.navigateByUrl(`/${route.snapshot.queryParams["ref"]}`)
                     }
                     else
                     {
-                        settings.set("session.token", response.data.id);
-                        settings.set("session.userId", response.data.user.id);
-
-                        if (route.snapshot.queryParams["ref"])
-                        {
-                            router.navigateByUrl(`/${route.snapshot.queryParams["ref"]}`)
-                        }
-                        else
-                        {
-                            router.navigateByUrl("/account");
-                        }
+                        router.navigateByUrl("/account");
                     }
+                }
 
-                    end(options);
-                },
-            });
+                end(options);
+            },
+        };
+    }
+
+    public static createNewApp(api: ApiService, form: FormComponent): FormConfig
+    {
+        return {
+            options: {
+                name: "Create new App",
+                groups: [
+                    {
+                        name: "default",
+                        inputs: [
+                            { label: "App Name", name: "name", type: "text", required: true },
+                            { label: "URL", name: "url", type: "url", required: true },
+                            { label: "Scopes", name: "scopes", type: "select", required: true, options: { multiple: true } },
+                        ],
+                    },
+                ],
+                submitButtonText: "Create",
+                showCancelButton: true,
+                hidden: true,
+            },
+            onSubmit: async (data, options, end) =>
+            {
+                const response = await api.createApp({
+                    name: data["name"],
+                    url: data["url"],
+                    scopes: data["scopes"],
+                });
+
+                if (response.errors)
+                {
+                    options.groups[0].inputs[0].error = response.errors.find(e => e.id.startsWith("app/name/"))?.message;
+                    options.groups[0].inputs[1].error = response.errors.find(e => e.id.startsWith("app/url/"))?.message;
+                }
+                else
+                {
+                    form.hide();
+                }
+
+                end(options);
+            },
+        };
+    }
+
+    public static updateProfile(api: ApiService, user: ISerializedUser, form: FormComponent, birthdayAsDate?: Date): FormConfig
+    {
+        return {
+            options: {
+                name: "Profile",
+                groups: [
+                    {
+                        name: "default",
+                        inputs: [
+                            { label: "First Name", name: "first-name", type: "text", required: true, autocomplete: "given-name", value: user.name.first },
+                            { label: "Last Name", name: "last-name", type: "text", required: true, autocomplete: "family-name", value: user.name.last },
+                            { label: "Email", name: "email", type: "email", required: true, autocomplete: "email", value: user.email },
+                        ],
+                    },
+                    {
+                        name: "Additional information",
+                        inputs: [
+                            { label: "Birthday", name: "birthday", type: "date", required: false, autocomplete: "bday", value: birthdayAsDate },
+                        ],
+                    },
+                ],
+                submitButtonText: "Update",
+                showCancelButton: true,
+                hidden: true,
+            },
+            onSubmit: async (data, options, end) =>
+            {
+                const birthday: Date = data["birthday"];
+
+                const response = await api.updateUser({
+                    name: {
+                        first: data["first-name"],
+                        last: data["last-name"],
+                    },
+                    email: data["email"],
+                    birthday: birthday && (`${
+                        birthday.getFullYear()
+                    }/${
+                        (birthday.getMonth() + 1).toString().padStart(2, "0")
+                    }/${
+                        (birthday.getDate()).toString().padStart(2, "0")
+                    }`),
+                });
+
+                if (response.errors)
+                {
+                    options.groups[0].inputs[0].error = response.errors.find(e => e.id.startsWith("user/name/first/"))?.message;
+                    options.groups[0].inputs[1].error = response.errors.find(e => e.id.startsWith("user/name/last/"))?.message;
+                    options.groups[0].inputs[2].error = response.errors.find(e => e.id.startsWith("user/email/"))?.message;
+
+                    options.groups[1].inputs[0].error = response.errors.find(e => e.id.startsWith("user/birthday/"))?.message;
+                }
+                else
+                {
+                    form.hide();
+                }
+
+                end(options);
+            },
+        };
+    }
 }
